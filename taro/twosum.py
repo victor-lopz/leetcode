4435f1cf58ca85367bcbf2d52fb6a999b857ef7a
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2025-12-13 20:55:25 +0100

    add: twosum

---
+++ taro/twosum.py
@@ -0,0 +1,18 @@
+def twosum(nums: list[int], target: int) -> list[int]:
+    no_solution = [-1,-1]
+    index_of = {}
+    for index, num in enumerate(nums):
+        looking_for = target - num
+        if looking_for in index_of:
+            return [index_of[looking_for], index]
+        index_of[num] = index
+    return no_solution
+
+def test():
+    nums = [-5,-1,0,0,2,2,3]
+    target = -2
+    answer = twosum(nums, target)
+    print(f"{answer = }")
+    expected = [0,6]
+    
+test()
