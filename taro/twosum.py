0de4f33dd382b66067376726060b2175dbb55bb7
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2025-12-13 22:43:13 +0100

    fix: use tuple instead of list for returning two indices

--- taro/twosum.py
+++ taro/twosum.py
@@ -1,10 +1,10 @@
-def twosum(nums: list[int], target: int) -> list[int]:
-    no_solution = [-1,-1]
+def twosum(nums: list[int], target: int) -> tuple[int, int]:
+    no_solution = (-1,-1)
     index_of = {}
     for index, num in enumerate(nums):
         looking_for = target - num
         if looking_for in index_of:
-            return [index_of[looking_for], index]
+            return (index_of[looking_for], index)
         index_of[num] = index
     return no_solution
 
